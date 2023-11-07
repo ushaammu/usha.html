5f4f15776cfdadcf8b9687f9fed7bd5dc8139072
ushaammu <ushausha0806@gmail.com>
2023-11-07 13:50:44 +0530

    usha

---
+++ jquery.js
@@ -0,0 +1,18 @@
+function fun1()
+{
+    $('#fade').fadeToggle();
+}
+//function fun1()
+//{
+    //document.getElementById('#div1').style;
+  // $("div").css('background-color','orange').fadeToggle();
+    //$("#p1").css('font-style','italic');
+    //$("#div1,#div3").css('background-color','blue');
+//}
+
+//function fun1(){
+//$("button").click(function()
+//{
+//$("#img1").css('width','500px','height','500px');
+//});
+//}
